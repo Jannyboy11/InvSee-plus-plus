ce50578b29b4d5d8a59a590eea7c13e07be2a770
Jan Boerman <janboerman95@gmail.com>
2023-01-08 01:10:58 +0100

    extend mirror test with custom mirror implementation

--- Spigot_Common/src/test/java/com/janboerman/invsee/spigot/template/MirrorTest.java
+++ Spigot_Common/src/test/java/com/janboerman/invsee/spigot/template/MirrorTest.java
@@ -29,7 +29,7 @@ public class MirrorTest {
         e_45 e_46 e_47 e_48 e_49 e_50 e_51 e_52 e_53
         """;
 
-    private static final PlayerInventorySlot[] EXPECTED_PLAYER_INVENTORY_TEMPLATE = new PlayerInventorySlot[] {
+    private static final PlayerInventorySlot[] DEFAULT_PLAYER_INVENTORY_TEMPLATE = new PlayerInventorySlot[] {
         PlayerInventorySlot.CONTAINER_00, PlayerInventorySlot.CONTAINER_01, PlayerInventorySlot.CONTAINER_02, PlayerInventorySlot.CONTAINER_03, PlayerInventorySlot.CONTAINER_04, PlayerInventorySlot.CONTAINER_05, PlayerInventorySlot.CONTAINER_06, PlayerInventorySlot.CONTAINER_07, PlayerInventorySlot.CONTAINER_08,
         PlayerInventorySlot.CONTAINER_09, PlayerInventorySlot.CONTAINER_10, PlayerInventorySlot.CONTAINER_11, PlayerInventorySlot.CONTAINER_12, PlayerInventorySlot.CONTAINER_13, PlayerInventorySlot.CONTAINER_14, PlayerInventorySlot.CONTAINER_15, PlayerInventorySlot.CONTAINER_16, PlayerInventorySlot.CONTAINER_17,
         PlayerInventorySlot.CONTAINER_18, PlayerInventorySlot.CONTAINER_19, PlayerInventorySlot.CONTAINER_20, PlayerInventorySlot.CONTAINER_21, PlayerInventorySlot.CONTAINER_22, PlayerInventorySlot.CONTAINER_23, PlayerInventorySlot.CONTAINER_24, PlayerInventorySlot.CONTAINER_25, PlayerInventorySlot.CONTAINER_26,
@@ -38,7 +38,7 @@ public class MirrorTest {
         PlayerInventorySlot.PERSONAL_00, PlayerInventorySlot.PERSONAL_01, PlayerInventorySlot.PERSONAL_02, PlayerInventorySlot.PERSONAL_03, PlayerInventorySlot.PERSONAL_04, PlayerInventorySlot.PERSONAL_05, PlayerInventorySlot.PERSONAL_06, PlayerInventorySlot.PERSONAL_07, PlayerInventorySlot.PERSONAL_08
     };
 
-    private static final EnderChestSlot[] EXPECTED_ENDER_CHEST_TEMPLATE = new EnderChestSlot[] {
+    private static final EnderChestSlot[] DEFAULT_ENDER_CHEST_TEMPLATE = new EnderChestSlot[] {
         EnderChestSlot.CONTAINER_00, EnderChestSlot.CONTAINER_01, EnderChestSlot.CONTAINER_02, EnderChestSlot.CONTAINER_03, EnderChestSlot.CONTAINER_04, EnderChestSlot.CONTAINER_05, EnderChestSlot.CONTAINER_06, EnderChestSlot.CONTAINER_07, EnderChestSlot.CONTAINER_08,
         EnderChestSlot.CONTAINER_09, EnderChestSlot.CONTAINER_10, EnderChestSlot.CONTAINER_11, EnderChestSlot.CONTAINER_12, EnderChestSlot.CONTAINER_13, EnderChestSlot.CONTAINER_14, EnderChestSlot.CONTAINER_15, EnderChestSlot.CONTAINER_16, EnderChestSlot.CONTAINER_17,
         EnderChestSlot.CONTAINER_18, EnderChestSlot.CONTAINER_19, EnderChestSlot.CONTAINER_20, EnderChestSlot.CONTAINER_21, EnderChestSlot.CONTAINER_22, EnderChestSlot.CONTAINER_23, EnderChestSlot.CONTAINER_24, EnderChestSlot.CONTAINER_25, EnderChestSlot.CONTAINER_26,
@@ -50,13 +50,13 @@ public class MirrorTest {
     @Test
     public void testInventory() {
         PlayerInventoryMirror template = new PlayerInventoryMirror(INVENTORY_TEMPLATE);
-        assertArrayEquals(EXPECTED_PLAYER_INVENTORY_TEMPLATE, template.getSlots());
+        assertArrayEquals(DEFAULT_PLAYER_INVENTORY_TEMPLATE, template.getSlots());
     }
 
     @Test
     public void testEnderChest() {
         EnderChestMirror template = new EnderChestMirror(ENDERCHEST_TEMPLATE);
-        assertArrayEquals(EXPECTED_ENDER_CHEST_TEMPLATE, template.getSlots());
+        assertArrayEquals(DEFAULT_ENDER_CHEST_TEMPLATE, template.getSlots());
     }
 
     @Test
@@ -73,4 +73,61 @@ public class MirrorTest {
         }
     }
 
+    private final Mirror<PlayerInventorySlot> reverse1st4Rows = new Mirror<PlayerInventorySlot>() {
+        @Override
+        public Integer getIndex(PlayerInventorySlot slot) {
+            int defaultIndex = slot.defaultIndex();
+            if (0 <= defaultIndex && defaultIndex < 9) {
+                return defaultIndex + 27;
+            } else if (9 <= defaultIndex && defaultIndex < 18) {
+                return defaultIndex + 9;
+            } else if (18 <= defaultIndex && defaultIndex < 27) {
+                return defaultIndex - 9;
+            } else if (27 <= defaultIndex && defaultIndex < 36) {
+                return defaultIndex - 27;
+            } else {
+                return defaultIndex;
+            }
+        }
+
+        @Override
+        public PlayerInventorySlot getSlot(int index) { //index is the 'warped' index.
+            if (0 <= index && index < 9) {
+                return PlayerInventorySlot.byDefaultIndex(index + 27);
+            } else if (9 <= index && index < 18) {
+                return PlayerInventorySlot.byDefaultIndex(index + 9);
+            } else if (18 <= index && index < 27) {
+                return PlayerInventorySlot.byDefaultIndex(index - 9);
+            } else if (27 <= index && index < 36) {
+                return PlayerInventorySlot.byDefaultIndex(index - 27);
+            } else if (36 <= index && index < 54) {
+                return PlayerInventorySlot.byDefaultIndex(index);
+            } else {
+                return null;
+            }
+        }
+    };
+
+    @Test
+    public void testReverse1st4RowsSound() {
+        for (PlayerInventorySlot slot : PlayerInventorySlot.values()) {
+            assertEquals(slot, reverse1st4Rows.getSlot(reverse1st4Rows.getIndex(slot)));
+        }
+    }
+
+    @Test
+    public void testReverse1st4RowsCorrectReversal() {
+        final PlayerInventorySlot[] expected = new PlayerInventorySlot[] {
+                PlayerInventorySlot.CONTAINER_27, PlayerInventorySlot.CONTAINER_28, PlayerInventorySlot.CONTAINER_29, PlayerInventorySlot.CONTAINER_30, PlayerInventorySlot.CONTAINER_31, PlayerInventorySlot.CONTAINER_32, PlayerInventorySlot.CONTAINER_33, PlayerInventorySlot.CONTAINER_34, PlayerInventorySlot.CONTAINER_35,
+                PlayerInventorySlot.CONTAINER_18, PlayerInventorySlot.CONTAINER_19, PlayerInventorySlot.CONTAINER_20, PlayerInventorySlot.CONTAINER_21, PlayerInventorySlot.CONTAINER_22, PlayerInventorySlot.CONTAINER_23, PlayerInventorySlot.CONTAINER_24, PlayerInventorySlot.CONTAINER_25, PlayerInventorySlot.CONTAINER_26,
+                PlayerInventorySlot.CONTAINER_09, PlayerInventorySlot.CONTAINER_10, PlayerInventorySlot.CONTAINER_11, PlayerInventorySlot.CONTAINER_12, PlayerInventorySlot.CONTAINER_13, PlayerInventorySlot.CONTAINER_14, PlayerInventorySlot.CONTAINER_15, PlayerInventorySlot.CONTAINER_16, PlayerInventorySlot.CONTAINER_17,
+                PlayerInventorySlot.CONTAINER_00, PlayerInventorySlot.CONTAINER_01, PlayerInventorySlot.CONTAINER_02, PlayerInventorySlot.CONTAINER_03, PlayerInventorySlot.CONTAINER_04, PlayerInventorySlot.CONTAINER_05, PlayerInventorySlot.CONTAINER_06, PlayerInventorySlot.CONTAINER_07, PlayerInventorySlot.CONTAINER_08,
+                PlayerInventorySlot.ARMOUR_BOOTS, PlayerInventorySlot.ARMOUR_LEGGINGS, PlayerInventorySlot.ARMOUR_CHESTPLATE, PlayerInventorySlot.ARMOUR_HELMET, PlayerInventorySlot.OFFHAND, PlayerInventorySlot.CURSOR, null, null, null,
+                PlayerInventorySlot.PERSONAL_00, PlayerInventorySlot.PERSONAL_01, PlayerInventorySlot.PERSONAL_02, PlayerInventorySlot.PERSONAL_03, PlayerInventorySlot.PERSONAL_04, PlayerInventorySlot.PERSONAL_05, PlayerInventorySlot.PERSONAL_06, PlayerInventorySlot.PERSONAL_07, PlayerInventorySlot.PERSONAL_08
+        };
+
+        for (int i = 0; i < expected.length; i++) {
+            assertEquals(expected[i], reverse1st4Rows.getSlot(i));
+        }
+    }
 }
