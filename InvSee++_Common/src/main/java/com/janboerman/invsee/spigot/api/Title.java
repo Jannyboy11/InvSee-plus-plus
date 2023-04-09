f6653a13d8d6ccc678e294e9963dbae887c4ecc0
Jannyboy11 <Jannyboy11@gmail.com>
2023-04-10 01:36:30 +0200

    title javadoc

--- InvSee++_Common/src/main/java/com/janboerman/invsee/spigot/api/Title.java
+++ InvSee++_Common/src/main/java/com/janboerman/invsee/spigot/api/Title.java
@@ -5,25 +5,42 @@ import com.janboerman.invsee.spigot.internal.ConstantTitle;
 
 import java.util.function.Function;
 
+/**
+ * Represents the title of a {@link SpectatorInventoryView}.
+ * The title string is computed from a {@link Target}.
+ */
 @FunctionalInterface
 public interface Title {
 
+    /**
+     * Get the title string for a given {@linkplain Target}.
+     * @param target the target
+     * @return the title string
+     */
     public String titleFor(Target target);
 
+    /**
+     * Create a title with a constant tile string.
+     * @param title the constant title string
+     * @return the Title
+     */
     public static Title of(String title) {
         if (title == null) return null;
         return new ConstantTitle(title);
     }
 
+    /** Convert a Function into a Title */
     public static Title of(Function<? super Target, ? extends String> function) {
         if (function == null) return null;
         return function::apply;
     }
 
+    /** Get the default main inventory title: {@code "<target>'s inventory"}*/
     public static Title defaultMainInventory() {
         return DefaultTitles.DEFAULT_MAIN_INVENTORY;
     }
 
+    /** Get the default main inventory title: {@code "<target>'s enderchest"}*/
     public static Title defaultEnderInventory() {
         return DefaultTitles.DEFAULT_ENDER_INVENTORY;
     }
