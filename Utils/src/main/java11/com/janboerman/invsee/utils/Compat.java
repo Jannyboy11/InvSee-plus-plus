30cc913db72e2ea66fe9e629c25ebc3b2351085d
Jan Boerman <jannyboy11@gmail.com>
2024-01-07 19:39:13 +0100

    Add java-11 Compat version.

--- Utils/src/main/java11/com/janboerman/invsee/utils/Compat.java
+++ Utils/src/main/java11/com/janboerman/invsee/utils/Compat.java
@@ -11,6 +11,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
@@ -20,50 +21,42 @@ public class Compat {
     private Compat() {}
 
     static void checkFromToIndex(int fromIndex, int toIndex, int size) {
-        if (fromIndex < 0) throw new IndexOutOfBoundsException("fromIndex must be 0 or greater.");
-        if (toIndex > size) throw new IndexOutOfBoundsException("toIndex must not be greater than size.");
-        if (fromIndex > toIndex) throw new IndexOutOfBoundsException("fromIndex must be smaller than toIndex.");
+        Objects.checkFromToIndex(fromIndex, toIndex, size);
     }
 
     static void checkIndex(int index, int size) {
-        if (index < 0 || index >= size) throw new IndexOutOfBoundsException("index out of range: " + index + ", range: [0-" + size + ")");
+        Objects.checkIndex(index, size);
     }
 
     public static <K, V> Entry<K, V> mapEntry(K key, V value) {
-        return new AbstractMap.SimpleImmutableEntry<>(key, value);
+        return Map.entry(key, value);
     }
 
     public static <K, V> Map<K, V> mapOfEntries(Entry<? extends K, ? extends V>... entries) {
-        LinkedHashMap<K, V> map = new LinkedHashMap<>();
-        for (Entry<? extends K, ? extends V> entry : entries) map.put(entry.getKey(), entry.getValue());
-        return Collections.unmodifiableMap(map);
+        return Map.ofEntries(entries);
     }
 
     public static Stream<String> lines(String string) {
-        return new BufferedReader(new StringReader(string)).lines();
+        return string.lines();
     }
 
     public static <T> List<T> listCopy(Collection<T> coll) {
-        return new ArrayList<>(coll);
+        return List.copyOf(coll);
     }
 
     public static <T> List<T> listOf(T... items) {
-        return Collections.unmodifiableList(Arrays.asList(items));
+        return List.of(items);
     }
 
     public static <T> List<T> emptyList() {
-        return Collections.emptyList();
+        return List.of();
     }
 
     public static <T> List<T> singletonList(T item) {
-        return Collections.singletonList(item);
+        return List.of(item);
     }
 
     public static <T> void ifPresentOrElse(Optional<T> optional, Consumer<? super T> action, Runnable emptyAction) {
-        if (optional.isPresent()) {
-            action.accept(optional.get());
-        } else {
-            emptyAction.run();
-        }
+        optional.ifPresentOrElse(action, emptyAction);
     }
 }
