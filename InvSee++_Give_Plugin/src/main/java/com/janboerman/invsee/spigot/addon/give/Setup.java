a1e53a003c8480550fb22a6991b79d269a180f45
Jannyboy11 <Jannyboy11@gmail.com>
2026-04-02 02:28:31 +0200

    Add give impl for CraftBukkit 26.1.1

--- InvSee++_Give_Plugin/src/main/java/com/janboerman/invsee/spigot/addon/give/Setup.java
+++ InvSee++_Give_Plugin/src/main/java/com/janboerman/invsee/spigot/addon/give/Setup.java
@@ -49,6 +49,8 @@ class SetupImpl implements Setup {
         SUPPORTED.registerSupportedVersion(() -> new Impl_1_21_9(), ServerSoftware.CRAFTBUKKIT_1_21_9, ServerSoftware.CRAFTBUKKIT_1_21_10, new ServerSoftware(MinecraftPlatform.PAPER, MinecraftVersion._1_21_9), new ServerSoftware(MinecraftPlatform.PAPER, MinecraftVersion._1_21_10));
         SUPPORTED.registerSupportedVersion(() -> new Impl_1_21_11(), ServerSoftware.CRAFTBUKKIT_1_21_11);
         SUPPORTED.registerSupportedVersion(() -> new Impl_Paper_1_21_11(), ServerSoftware.PAPER_1_21_11);
+        SUPPORTED.registerSupportedVersion(() -> new Impl_26_1_1(), ServerSoftware.CRAFTBUKKIT_26_1_1, new ServerSoftware(MinecraftPlatform.CRAFTBUKKIT, MinecraftVersion._26_1));
+        // TODO Paper 26.1.1
         final SetupProvider glowstoneProver = () -> new Impl_Glowstone();
         final MinecraftVersion[] minecraftVersions = MinecraftVersion.values();
         for (int idx = MinecraftVersion._1_8.ordinal(); idx < MinecraftVersion._1_12_2.ordinal(); idx++) {
@@ -65,6 +67,12 @@ class SetupImpl implements Setup {
     @Override
     public GiveApi getGiveApi() {
         return api;
+    }
+}
+
+class Impl_26_1_1 extends SetupImpl {
+    Impl_26_1_1() {
+        super(com.janboerman.invsee.spigot.addon.give.impl_26_1_1.GiveImpl.INSTANCE)
     }
 }
 
